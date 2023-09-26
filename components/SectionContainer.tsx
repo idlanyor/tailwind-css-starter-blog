ae0616a0c8b840c15e487ccc5812888090cf060a
roynaldi3301 <prasbhara0604@gmail.com>
2023-09-26 22:02:06 +0700

    SECTION LINT

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,7 +5,5 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return (
-    <section className=" mx-5 px-4 sm:px-6 xl:mx-10 xl:px-0">{children}</section>
-  )
+  return <section className=" mx-5 px-4 sm:px-6 xl:mx-10 xl:px-0">{children}</section>
 }
