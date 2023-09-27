80824202131c3010205d46a79b1a940ac5a89edf
roynaldi3301 <prasbhara0604@gmail.com>
2023-09-27 19:51:19 +0700

    tambah api rest

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,6 +18,8 @@ const siteMetadata = {
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
+  linkLol: 'https://api.lolhuman.xyz',
+  apikeyLol: '',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
