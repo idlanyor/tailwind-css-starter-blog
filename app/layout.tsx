4ce9e7680b1e67efac4a6ceffa411f96ceb04bc7
idlanyor <prasbhara0604@gmail.com>
2024-06-12 11:47:19 +0700

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <script>(function(s,u,z,p){s.src = u, s.setAttribute('data-zone', z), p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7602578,document.body||document.documentElement)</script>
+     
       <script src="https://alwingulla.com/88/tag.min.js" data-zone="72318" async data-cfasync="false"></script>
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
